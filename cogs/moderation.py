fd60346e5a54b865ddf1b3c868fb185fec4c316d
IlikeFivem <92817827+IlikeFivem@users.noreply.github.com>
2021-10-19 20:56:31 +0100

    Added mute command in moderation.py in folder cogs

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,3 +1,4 @@
+from typing import overload
 import discord
 from discord.ext import commands
 
@@ -13,6 +14,7 @@ class moderation(commands.Cog):
         embed.add_field(name='Reason', value=f'{reason}', inline=False)
         await ctx.send(embed)
         memberEmbed = discord.Embed()
+        memberEmbed.title('Kicked')
         memberEmbed.add_field('Staff Member', value=f'{ctx.author}', inline=False)
         memberEmbed.add_field('Reason', value=f'{reason}', inline=False)
         memberEmbed.add_field('Guild', value=f'{ctx.guild.name}', inline=False)
@@ -27,11 +29,48 @@ class moderation(commands.Cog):
         embed.add_field(name='Reason', value=f'{reason}', inline=False)
         await ctx.send(embed)
         memberEmbed = discord.Embed()
+        memberEmbed.title('Banned')
         memberEmbed.add_field(name='Staff Member', value=f'{ctx.author}', inline=False)
         memberEmbed.add_field(name='Reason', value=f'{reason}', inline=False)
         memberEmbed.add_field(name='Guild', value=f'{ctx.guild.name}', inline=False)
         await member.send(memberEmbed)
         await member.kick(reason=reason)
+
+    @commands.command()
+    async def mute(self, ctx, member : discord.Member, *, reason=None):
+        role = discord.utils.find(lambda r: r.name == 'muted', ctx.guild.roles)
+        if role == None:
+            await ctx.guild.create_role(name="muted")
+            embed = discord.Embed()
+            embed.add_field(name='Member Muted', value=f'{member.display_name}', inline=False)
+            embed.add_field(name='Reason', value=f'{reason}', inline=False)
+            await ctx.send(embed=embed)
+            memberEmbed = discord.Embed(title="Muted")
+            memberEmbed.add_field(name='Staff Member', value=f'{ctx.author}', inline=False)
+            memberEmbed.add_field(name='Reason', value=f'{reason}', inline=False)
+            memberEmbed.add_field(name='Guild', value=f'{ctx.guild.name}', inline=False)
+            await member.send(embed=memberEmbed)
+            await member.add_roles(role)
+            text_channel_list = []
+            for server in ctx.guilds:
+                for channel in server.channels:
+                    if str(channel.type) == 'text':
+                        text_channel_list.append(channel)
+            overwrite = discord.PermissionOverwrite()
+            overwrite.send_messages = False
+            overwrite.read_messages = True
+            await channel.set_permissions(role, overwrite)
+        else:
+            embed = discord.Embed()
+            embed.add_field(name='Member Muted', value=f'{member.display_name}', inline=False)
+            embed.add_field(name='Reason', value=f'{reason}', inline=False)
+            await ctx.send(embed=embed)
+            memberEmbed = discord.Embed(title="Muted")
+            memberEmbed.add_field(name='Staff Member', value=f'{ctx.author}', inline=False)
+            memberEmbed.add_field(name='Reason', value=f'{reason}', inline=False)
+            memberEmbed.add_field(name='Guild', value=f'{ctx.guild.name}', inline=False)
+            await member.send(embed=memberEmbed)
+            await member.add_roles(role)
 
 def setup(bot):
     bot.add_cog(moderation(bot))
