575d653d46b2422fd5bd8d8a59e307dc6542a1ce
IlikeFivem <92817827+IlikeFivem@users.noreply.github.com>
2021-10-19 20:22:13 +0100

    Made a loop for cogs
    Made a loop for cogs just so i don't have to manually add them.

--- bot.py
+++ bot.py
@@ -1,13 +1,22 @@
+#Imports
 import discord
 from discord.ext import commands
 import config
+import os
 
+#Init bot
 bot = commands.Bot(command_prefix='!')
 
+#Notify bot is online
 @bot.event
 async def on_ready():
     print('The bot is online!')
 
-bot.load_extension("cogs.test")
+#Load cogs
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
+
+# Run bot
 bot.run(config.TOKEN)
