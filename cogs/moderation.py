3edc6b631355a643b017ab3bfaddbc5705afa33a
IlikeFivem <92817827+IlikeFivem@users.noreply.github.com>
2021-10-19 20:35:34 +0100

    Added ban command in moderation.py in folder cogs

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -17,7 +17,21 @@ class moderation(commands.Cog):
         memberEmbed.add_field('Reason', value=f'{reason}', inline=False)
         memberEmbed.add_field('Guild', value=f'{ctx.guild.name}', inline=False)
         await member.send(memberEmbed)
-        await member.ban(reason=reason)
+        await member.kick(reason=reason)
+    
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member : discord.Member, *, reason=None):
+        embed = discord.Embed()
+        embed.add_field(name='Member Kicked', value=f'{member.display_name}', inline=False)
+        embed.add_field(name='Reason', value=f'{reason}', inline=False)
+        await ctx.send(embed)
+        memberEmbed = discord.Embed()
+        memberEmbed.add_field(name='Staff Member', value=f'{ctx.author}', inline=False)
+        memberEmbed.add_field(name='Reason', value=f'{reason}', inline=False)
+        memberEmbed.add_field(name='Guild', value=f'{ctx.guild.name}', inline=False)
+        await member.send(memberEmbed)
+        await member.kick(reason=reason)
 
 def setup(bot):
     bot.add_cog(moderation(bot))
